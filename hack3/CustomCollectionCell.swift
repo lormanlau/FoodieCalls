90e21a8c1028bdf009aa4a2d6bb898b9c3e802fe
Lorman Lau <lormanlau@gmail.com>
2017-09-19 16:41:03 -0700

    added the API calls

---
+++ hack3/CustomCollectionCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCollectionCell.swift
+//  hack3
+//
+//  Created by Lorman Lau on 9/19/17.
+//  Copyright © 2017 havisha tiruvuri. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomCollectionCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+}
